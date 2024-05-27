be2d8bc1dc5f6f57530b4db4096b306761b210e4
biahoover <biahoover.packs@gmail.com>
2024-05-26 21:26:39 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// Função para salvar as configurações
-function saveSettings() {
-    // Aqui você pode implementar a lógica para salvar as configurações
-    // Recupere os valores dos campos e faça o que for necessário
-    alert('Configurações salvas com sucesso!');
-}
-
-// Adiciona um evento de clique ao botão "Salvar Configurações"
-document.getElementById('save-button').addEventListener('click', saveSettings);
