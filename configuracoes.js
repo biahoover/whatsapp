444c11065dbd12b1a37922231489a235fd7d4241
biahoover <biahoover.packs@gmail.com>
2024-05-27 13:20:52 -0300

    Update and rename buttonSettings.js to configuracoes.js

---
+++ configuracoes.js
@@ -0,0 +1,43 @@
+{
+  "buttonSettings": {
+    "whatsapp": {
+      "text": "FALE COMIGO NO WHATSAPP ♥",
+      "color": "#25D366",
+      "textColor": "#ffffff",
+      "startTime": 13,
+      "endTime": 23,
+      "pulse": false,
+      "pulseStart": 8,
+      "pulseEnd": 23,
+      "visibility": true,
+      "visibilityStart": 0,
+      "visibilityEnd": 24
+    },
+    "telegram": {
+      "text": "CANAL NO TELEGRAM<br>Inscreva-se grátis",
+      "color": "#0088cc",
+      "textColor": "#ffffff",
+      "startTime": 0,
+      "endTime": 24,
+      "pulse": true,
+      "pulseStart": 8,
+      "pulseEnd": 20,
+      "visibility": true,
+      "visibilityStart": 0,
+      "visibilityEnd": 24
+    },
+    "instagram": {
+      "text": "Visite meu Instagram",
+      "color": "linear-gradient(45deg, #405DE6, #5851DB, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #F77737, #FCAF45)",
+      "textColor": "#ffffff",
+      "startTime": 0,
+      "endTime": 24,
+      "pulse": false,
+      "pulseStart": 0,
+      "pulseEnd": 8,
+      "visibility": true,
+      "visibilityStart": 0,
+      "visibilityEnd": 24
+    }
+  }
+}
